d009beaa348beb62cfbcbf9edeab55b76ad84ae0
Ian Cargill <ianc@zillowgroup.com>
2021-09-20 16:16:33 -0700

    organize

---
+++ Examples/ReactStateProps.js
@@ -0,0 +1,26 @@
+// React Lifecycle
+
+// componentDidMount
+
+
+
+//render
+
+
+
+//constructor
+
+
+// compontentWillUnmount
+
+
+//React Updates
+
+
+
+/*
+Props are handled / updated outside of component.  
+State is handled within a componenet
+State -caused re render.
+
+*/
